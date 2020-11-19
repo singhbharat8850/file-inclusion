840c4cc91dd8c6349559140816811e030b8ce8ec
Bharat Singh <singh.bharat8850@gmail.com>
2020-11-19 17:53:08 +0530

    Create content.php

---
+++ content.php
@@ -0,0 +1,334 @@
+<section class="hero">
+  <p class="text-center">
+  <a href="#">START A COURSE</a>
+  </p>
+</section>
+<div class="block-on-banner">
+  <div class="block-on-bannercontainer">
+    <div class="container">
+      <div class="service-area">
+          <div class="block1">
+            <div class="block1content">
+              <h3><a href="#">Scholarship Facility</a></h3>
+              <p>Tmply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <div class="block1logo">
+              <img src="images/grad.png" width="" height="" alt="" title="">
+            </div>
+          </div>
+          <div class="block2">
+            <div class="block2content">
+              <h3><a href="#">Skilled Lecturers</a></h3>
+              <p>Tmply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <div class="block2logo">
+              <img src="images/human.png">
+            </div>
+          </div>
+          <div class="block3">
+            <div class="block3content">
+              <h3><a href="#">Book Store</a></h3>
+              <p>Tmply dummy text of the printing and typesetting industry.</p>
+            </div>
+            <div class="block3logo">
+              <img src="images/book.svg">
+            </div>
+          </div><br class="clear">
+      </div>
+    </div>
+  </div>
+</div>
+<section class="content1">
+  <div class="container">
+    <h1>Welcome to Academics</h1>
+    <p>Tmply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+    standard dummy text ever since the 1500s, when an unknown printer took.</p>
+    <img src="images/content1.jpeg">
+  </div>
+</section>
+<section class="content2">
+  <div class="container">
+    <h2>Featured Courses</h2>
+    <div class="content22">
+      <div class="courses1">
+        <div>
+          <img src="images/courses1.jpg">
+        </div>
+        <div class="courses11">
+          <h3><a href="#">Basic Philosophy</a></h3>
+          <p>Tmply dummy text of the printing and typesetting industry.</p>
+          <div>
+            <table cellspacing="0">
+              <tr>
+                <td style="border:none">1 Year Course</td>
+                <td>50 class</td>
+                <td>10am - 11am Timing</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="courses1">
+        <div>
+          <img src="images/courses1.jpg">
+        </div>
+        <div class="courses11">
+          <h3><a href="#">Regular MBA</a></h3>
+          <p>Tmply dummy text of the printing and typesetting industry.</p>
+          <div>
+            <table cellspacing="0">
+            <tr>
+              <td>1 Year Course</td>
+              <td>50 class</td>
+              <td>11am - 12am Timing</td>
+            </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="courses1">
+        <div>
+          <img src="images/courses1.jpg">
+        </div>
+        <div class="courses11">
+          <h3><a href="#">IELTS</a></h3>
+          <p>Tmply dummy text of the printing and typesetting industry.</p>
+          <div>
+            <table cellspacing="0">
+              <tr>
+                <td style="border:none">8 months Course</td>
+                <td>39 class</td>
+                <td>12pm - 1pm Timing</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="courses1">
+        <div>
+          <img src="images/courses1.jpg">
+        </div>
+        <div class="courses11">
+        <h3><a href="#">GMAT</a></h3>
+          <p>Tmply dummy text of the printing and typesetting industry.</p>
+          <div>
+            <table cellspacing="0">
+              <tr>
+                <td style="border:none">4 months Course</td>
+                <td>20 class</td>
+                <td>1pm - 2pm Timing</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div><br class="clear">
+    </div>
+  </div>
+</section>
+<section class="content3">
+  <div class="content3cover">
+    <div class="container">
+      <div class="videoarea">
+        <div class="videoarea1">
+          <h2>Watch Campus Video Life Tour</h2>
+          <p>Tmply dummy text of the printing and typesetting industry.Tmply dummy text of<br> the printing and typesetting industry.</p>
+          <a href="https://www.youtube.com/watch?v=_Nq4Z5i7lcs"><img src="images/video.svg"></a> 
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="content4">
+  <div class="container">
+    <h5>Our Skilled Lecturers</h5>
+    <div class="content4lecturers">
+      <div class="lecturer1">
+        <a href="#"><img src="images/prof.jpeg"></a>
+        <h3>NAME</h3>
+        <span>Senior Physics Lecturer</span>
+        <ul>
+          <li>
+            <a href="#"><img src="images/mail.svg"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/linkedin.svg"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/twitter.svg"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/fb.svg"></a>
+          </li><br class="clear">
+        </ul>
+      </div>
+      <div class="lecturer1">
+        <a href="#"><img src="images/prof.jpeg"></a>
+        <h3>NAME</h3>
+        <span>Senior Physics Lecturer</span>
+        <ul>
+          <li>
+            <a href="#"><img src="images/mail.svg" class="img-responsive"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/linkedin.svg"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/twitter.svg"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/fb.svg"></a>
+          </li><br class="clear">
+        </ul>
+      </div>
+      <div class="lecturer1">
+        <a href="#"><img src="images/prof.jpeg"></a>
+        <h3>NAME</h3>
+        <span>Senior Physics Lecturer</span>
+        <ul>
+          <li>
+            <a href="#"><img src="images/mail.svg" class="img-responsive"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/linkedin.svg"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/twitter.svg"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/fb.svg"></a>
+          </li><br class="clear">
+        </ul>
+      </div>
+      <div class="lecturer1">
+        <a href="#"><img src="images/prof.jpeg"></a>
+        <h3>NAME</h3>
+        <span>Senior Physics Lecturer</span>
+        <ul>
+          <li>
+            <a href="#"><img src="images/mail.svg" class="img-responsive"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/linkedin.svg"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/twitter.svg"></a>
+          </li>
+          <li>
+            <a href="#"><img src="images/fb.svg"></a>
+          </li><br class="clear">
+        </ul>
+      </div><br class="clear">
+    </div>
+  </div>
+</section>
+<section class="content6">
+  <div class="content66">
+    <div class="container">
+      <div class="content666">
+        <div class="content6661">
+          <div class="content66611">
+            80
+          </div>
+          <div class="content66612">
+            PROFESSIONAL<br>TEACHERS
+          </div>
+        </div>
+        <div class="content6661">
+          <div class="content66611">
+            80
+          </div>
+          <div class="content66612">
+            NEW COURSES<br>EVERY YEAR
+          </div>
+        </div>
+        <div class="content6661">
+          <div class="content66611">
+            40
+          </div>
+          <div class="content66612">
+            WORKSHOPS<br>EACH YEAR
+          </div>
+        </div>
+        <div class="content6661">
+          <div class="content66611">
+            <span>18</span>
+          </div>
+          <div class="content66612">
+            CONFERENCES<br>EACH YEAR
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="content7">
+<div class="container">
+  <h2>What Our Students Say</h2>
+  <div class="content77">
+    <div class="content771">
+      <h3>
+      <a href="#">Rosy Janner</a>
+      </h3>
+      <div class="content771image">
+        <img src="images/person1.jpeg">
+      </div>
+      <span class="span1">UI Designer</span>
+      <ul class="rating-wrapper">
+        <li><img src="images/star.jpeg"></li>
+        <li><img src="images/star.jpeg"></li>
+        <li><img src="images/star.jpeg"></li>
+        <li><img src="images/star.jpeg"></li>
+        <li><img src="images/star.jpeg"></li>
+      </ul>
+      <div class="content771text">
+        Pellentese turpis dignissim amet area ducation process facilitating Knowledge.
+      </div>
+    </div>
+    <div class="content771">
+      <h3>
+      <a href="#">Mark Brown</a>
+      </h3>
+      <div class="content771image">
+        <img src="images/person.jpeg">
+      </div>
+      <span class="span1">Manager</span>
+      <ul class="rating-wrapper">
+        <li><img src="images/star.jpeg"></li>
+        <li><img src="images/star.jpeg"></li>
+        <li><img src="images/star.jpeg"></li>
+        <li><img src="images/star.jpeg"></li>
+        <li><img src="images/star.jpeg"></li>
+      </ul>
+      <div class="content771text">
+        Pellentese turpis dignissim amet area ducation process facilitating Knowledge.
+      </div>
+    </div><br class="clear">
+  </div>
+</div>
+</section>
+<section class="content9">
+<div class="container">
+  <div class="content99">
+    <div class="content991">
+      <a href="#">
+        <img src="images/sponsors.jpeg">
+      </a>
+    </div>
+    <div class="content991">
+      <a href="#">
+        <img src="images/sponsor1.jpeg">
+      </a>
+    </div>
+    <div class="content991">
+      <a href="#">
+        <img src="images/sponsor.jpeg">
+      </a>
+    </div>
+    <div class="content991">
+      <a href="#">
+        <img src="images/sponsors.jpeg">
+      </a>
+    </div><br class="clear">
+  </div>
+</div>
+</section>
