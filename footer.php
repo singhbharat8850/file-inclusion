81c3a144272f933e92799b6ea9c1a76165036c99
Bharat Singh <singh.bharat8850@gmail.com>
2020-11-19 17:52:43 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,169 @@
+<footer>
+<div class="container">
+  <div class="subfooter">
+    <div class="footerbox1">
+      <a href="index.html">
+        <img src="images/logo.png">
+      </a>
+      <div class="footerabout">
+      <p>Praesent vel rutrum purus. Nam vel dui eu sus duis dignissim dignissim. Suspenetey disse.</p>
+      </div>
+      <ul class="footer-social">
+        <li>
+          <a href="#">
+            <img src="images/fb.svg">
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="images/twitter.svg">
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="images/linkedin.svg">
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="images/spotify.png">
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="images/pinterest.svg">
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="images/gplus.svg">
+          </a>
+        </li><br class="clear">
+      </ul>
+    </div>
+    <div class="footerbox2">
+      <h3>FEATURED LINKS</h3>
+      <div>
+      <ul class="featuredlinks">
+        <li>
+        <a href="#"><span>&#62; </span>Graduation</a>
+        </li>
+        <li>
+        <a href="#"><span>&#62; </span>Admissions</a>
+        </li>
+        <li>
+        <a href="#"><span>&#62; </span>International</a>
+        </li>
+        <li>
+        <a href="#"><span>&#62; </span>FAQs</a>
+        </li><br class="clear">
+      </ul>
+      </div>
+      <div>
+      <ul class="featuredlinks">
+        <li>
+        <a href="#"><span>&#62; </span>Courses</a>
+        </li>
+        <li>
+        <a href="#"><span>&#62; </span>About Us</a>
+        </li>
+        <li>
+        <a href="#"><span>&#62; </span>Book Store</a>
+        </li>
+        <li>
+        <a href="#"><span>&#62; </span>Alumni</a>
+        </li><br class="clear">
+      </ul>
+      </div>
+    </div>
+    <div class="footerbox3">
+    <h4>INFORMATION</h4>
+    <ul>
+      <li>
+        <img src="images/phone.svg">
+        <a href="Phone(01)800433633">(01)800433633</a>
+      </li>
+      <li>
+        <img src="images/phone.svg">
+        <a href="mailto:info@web.com">info@bostonea.com</a>
+      </li><br class="clear">
+    </ul>
+    <h5>Newsletter</h5>
+    <input class="input" type="text" placeholder="enter your e-mail here">
+    <span></span>
+    </div>
+    <div class="footerbox4">
+      <h4>Flickr Photos</h4>
+      <ul>
+         <li>
+           <a href="#">
+             <img src="images/flickr.jpg">
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             <img src="images/flickr.jpg">
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             <img src="images/flickr.jpg">
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             <img src="images/flickr.jpg">
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             <img src="images/flickr.jpg">
+           </a>
+         </li>
+         <li>
+           <a href="#">
+             <img src="images/flickr.jpg">
+           </a>
+         </li><br class="clear">
+      </ul>
+    </div><br class="clear">
+  </div>
+</div>
+<div class="bottombar">
+  <div class="container">  
+    <div class="row">
+      <div class="row1">
+        <p>&copy; 2017 Academics All Rights Reserved.    Designed by
+        <a target="blank" href="#" rel="nofollow">ABC</a>
+        </p>
+      </div>
+      <div class="row2">
+        <ul>
+          <li>
+          <a href="#">
+          <img src="images/visa.png">
+          </a>
+          </li>
+          <li>
+          <a href="#">
+          <img src="images/discover.png">
+          </a>
+          </li>
+          <li>
+          <a href="#">
+          <img src="images/paypal.png">
+          </a>
+          </li>
+          <li>
+          <a href="#">
+          <img src="images/mastercard.png">
+          </a>
+          </li><br class="clear">
+        </ul>
+      </div><br class="clear">
+    </div>
+  </div>
+</div>
+</footer>
+</body>
+</html>
