db2a8d19bc1b18e57bdb203a0842427703c4da8d
Bharat Singh <singh.bharat8850@gmail.com>
2020-11-19 17:52:23 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ACADEMICS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+  <div class="container">
+    <div class="logo">
+      <a href="#"><img src="images/logo.png"></a>
+    </div>
+    <nav class="navbar">
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">PAGES</a></li>
+        <li><a href="#">COURSES</a></li>
+        <li><a href="#">RESEARCH</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">GALLERY</a></li>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
+    </nav>
+    <div class="applybutton">
+      <a href="#">APPLY NOW</a>
+    </div>
+  </div>
+</header>
